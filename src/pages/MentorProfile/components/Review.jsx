edb5e75b210aa21161d388d940b0a97b92c7d727
Jayden <Jayden7evans@gmail.com>
2023-11-21 20:06:08 -0800

    Design of mentor profile finished

---
+++ src/pages/MentorProfile/components/Review.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Review = () => {
+  return (
+    <div className='w-5/12 my-5 mr-5'>
+            <div className='flex flex-col gap-2'>
+                <div className='flex w-full justify-between'>
+                    <p className='font-semibold'>First Name</p>
+                    <p>X/X/2023</p>
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Tenetur delectus dolore rem nemo magni autem iure ipsa assumenda,
+                    earum mollitia impedit veritatis. Pariatur numquam, rem nisi
+                    cumque officia labore modi.
+                </p>
+            </div>
+    </div>
+  )
+}
+
+export default Review
