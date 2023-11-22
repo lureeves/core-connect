1e59fe8eb00de9a68f9d9f3ef015f48937a6e69a
lureeves <ljr77777@gmail.com>
2023-11-21 16:49:47 -0800

    core value test refactoring

--- src/pages/CoreValuesTest/components/TestCard.jsx
+++ src/pages/CoreValuesTest/components/TestCard.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../CoreValueTest.css';
 import { ActionButton }  from './subComponents/Buttons.jsx'
+<<<<<<< HEAD
 import { StartTest, InitialValues, TenValues, FiveValues, ThreeValues } from './subComponents/ValueGridRender.jsx';
 
 /**
@@ -9,6 +10,12 @@ import { StartTest, InitialValues, TenValues, FiveValues, ThreeValues } from './
  */
 const TestCard = () => {
     // List of core values available for selection.
+=======
+import { InitialValues, TenValues, FiveValues, ThreeValues } from './subComponents/ValueGridRender.jsx';
+
+// TestCard component handles the core values test
+const TestCard = () => {
+>>>>>>> b525b17 (added documentation to TestCard.jsx)
     const coreValues = [
     "Acheivement", "Discipline", "Generosity", "Knowledge", "Respect",
     "Adaptability", "Empathy", "Hard work", "Leadership", "Responsibility",
@@ -19,6 +26,7 @@ const TestCard = () => {
     "Curiosity", "Freedom", "Kindness", "Recognition", "Wisdom"
     ];
 
+<<<<<<< HEAD
     // State hooks for managing the values selected in different steps.
     const [stateOneValues, setStateOneValues] = useState([]);
     const [stateTwoValues, setStateTwoValues] = useState([]);
@@ -50,15 +58,46 @@ const TestCard = () => {
             case 1: setStateOneValues(newValues); break;
             case 2: setStateTwoValues(newValues); break;
             case 3: setStateThreeValues(newValues); break;
+=======
+    // State for storing selected values at each step
+    const [stateOneValues, setStateOneValues] = useState([]);
+    const [stateTwoValues, setStateTwoValues] = useState([]);
+    const [stateThreeValues, setStateThreeValues] = useState([]);
+    const [shownValues, setShownValues] = useState([]); // Shown for that page (selected values from the last step)
+    const [selectedValues, setSelectedValues] = useState([]); // Currently selected values
+    const [step, setStep] = useState(1);
+
+    // Handles toggling value selection
+    const handleValueClick = (value) => {
+        if (selectedValues.includes(value)) {
+            setSelectedValues(selectedValues.filter(v => v !== value));
+        } else {
+            setSelectedValues([...selectedValues, value]);
+        }
+    };
+    
+    // Handles next button click logic
+    const handleNext = () => {
+        let newValues = [];
+        const newStep = step + 1;
+        switch (step) {
+            case 1: newValues = selectedValues; setStateOneValues(newValues);break;
+            case 2: newValues = selectedValues; setStateTwoValues(newValues);break;
+            case 3: newValues = selectedValues; setStateThreeValues(newValues); break;
+>>>>>>> b525b17 (added documentation to TestCard.jsx)
         }
         setShownValues(newValues);
         setSelectedValues([]);
         setStep(step + 1);
     };   
     
+<<<<<<< HEAD
     /**
      * Handles the logic for moving back to the previous step of the test.
      */
+=======
+    // Handles back button click logic
+>>>>>>> b525b17 (added documentation to TestCard.jsx)
     const handleBack = () => {
         const newStep = step - 1;
         switch (newStep) {
@@ -69,6 +108,7 @@ const TestCard = () => {
         setStep(newStep);
     };
     
+<<<<<<< HEAD
     /**
      * Handles the logic for submitting the final selected values.
      */
@@ -92,6 +132,20 @@ const TestCard = () => {
             case 2: return <TenValues {...{stateOneValues, selectedValues, handleValueClick}} />;
             case 3: return <FiveValues {...{stateTwoValues, selectedValues, handleValueClick}} />;
             case 4: return <ThreeValues {...{stateThreeValues, selectedValues, handleValueClick}} />;
+=======
+    // Handles submit button click logic 
+    const handleSubmit = () => {
+        localStorage.setItem('selectedValues', JSON.stringify(selectedValues));
+    };
+    
+    // Render the correct grid based on the current step
+    const GridRender = () => {
+        switch(step) {
+            case 1: return <InitialValues coreValues={coreValues} selectedValues={selectedValues} handleValueClick={handleValueClick} />;
+            case 2: return <TenValues stateOneValues={stateOneValues} selectedValues={selectedValues} handleValueClick={handleValueClick} />;
+            case 3: return <FiveValues stateTwoValues={stateTwoValues} selectedValues={selectedValues} handleValueClick={handleValueClick} />;
+            case 4: return <ThreeValues stateThreeValues={stateThreeValues} selectedValues={selectedValues} handleValueClick={handleValueClick} />;
+>>>>>>> b525b17 (added documentation to TestCard.jsx)
             default: return <DefaultComponent />;
         }
     }
@@ -101,12 +155,19 @@ const TestCard = () => {
      * Determines if the 'Next' button should be enabled based on the current step and selection.
      */
     const NextConditional = () => {
+<<<<<<< HEAD
         return (
             (step === 1 && selectedValues.length > 10) ||
             (step === 2 && selectedValues.length === 10) ||
             (step === 3 && selectedValues.length === 5) ||
             (step === 4 && selectedValues.length === 3)
         );
+=======
+        return (step === 1 && selectedValues.length > 10) || 
+               (step === 2 && selectedValues.length === 10) || 
+               (step === 3 && selectedValues.length === 5) || 
+               (step === 4 && selectedValues.length === 3);
+>>>>>>> b525b17 (added documentation to TestCard.jsx)
     }
 
     // Effect hook to trigger updates on step change.
@@ -115,6 +176,7 @@ const TestCard = () => {
     }, [step, shownValues]);
     
     return (
+<<<<<<< HEAD
         <>
             <div className='w-9/12 py-14 px-24 mx-auto text-lg border-transparent rounded-xl shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px] clearfix'>
                 {GridRender()}
@@ -124,6 +186,20 @@ const TestCard = () => {
             </div>
         </>
     );
+=======
+        <> <div className='w-9/12 py-14 px-24 mx-auto text-lg border-transparent rounded-xl shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px] clearfix'>
+            {/* Grid of core values */}
+            {GridRender()}
+
+            {/* Back button only after first page */}
+            {step >= 2 && <ActionButton type="back" onClick={handleBack} isEnabled={'True'} />}
+            {/* Next button on every page except last */}
+            {step <= 3 && <ActionButton type="next" onClick={handleNext} isEnabled={NextConditional()} />}
+            {/* Submit button only on last page*/}
+            {step === 4 && <ActionButton type="submit" onClick={handleSubmit} isEnabled={NextConditional()} />}
+        </div> </>
+  );
+>>>>>>> b525b17 (added documentation to TestCard.jsx)
 };
 
 export default TestCard;
--- src/pages/CoreValuesTest/components/subComponents/ValueGridRender.jsx
+++ src/pages/CoreValuesTest/components/subComponents/ValueGridRender.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // StepComponents.jsx
 import React from 'react';
 import { CoreValueButton, ActionButton } from './Buttons.jsx';
@@ -30,6 +31,17 @@ export const StartTest = ({ _, __, handleNext }) => {
  * @param {Array} props.coreValues - Array of core values to be displayed totalling 35.
  * @param {Array} props.selectedValues - Array of currently selected values.
  * @param {Function} props.handleValueClick - Function to handle the selection of a value.
+=======
+import React from 'react';
+import { CoreValueButton } from './Buttons.jsx'
+
+/**
+ * InitialValues component renders the initial set of core values for the user to select from.
+ * @param {Object} props - The properties passed to the component.
+ * @param {Array} props.coreValues - The list of all core values.
+ * @param {Array} props.selectedValues - The list of currently selected values.
+ * @param {Function} props.handleValueClick - The function to handle click events on the values.
+>>>>>>> b525b17 (added documentation to TestCard.jsx)
  */
 export const InitialValues = ({ coreValues, selectedValues, handleValueClick }) => {
     return (
@@ -41,7 +53,11 @@ export const InitialValues = ({ coreValues, selectedValues, handleValueClick }) 
                         key={value}
                         value={value}
                         isSelected={selectedValues.includes(value)}
+<<<<<<< HEAD
                         onClick={() => handleValueClick(value)}
+=======
+                        onClick={handleValueClick}
+>>>>>>> b525b17 (added documentation to TestCard.jsx)
                     />
                 ))}
             </div>
@@ -50,11 +66,19 @@ export const InitialValues = ({ coreValues, selectedValues, handleValueClick }) 
 };
 
 /**
+<<<<<<< HEAD
  * Component to display and allow selection from the first set of chosen values.
  * @param {Object} props - Component props.
  * @param {Array} props.stateOneValues - Array of values selected totalling the first step of selected values.
  * @param {Array} props.selectedValues - Array of currently selected values, specifically 10.
  * @param {Function} props.handleValueClick - Function to handle the selection of a value.
+=======
+ * TenValues component renders the selected values for the user to narrow down to 10.
+ * @param {Object} props - The properties passed to the component.
+ * @param {Array} props.stateOneValues - The list of values selected in the first step.
+ * @param {Array} props.selectedValues - The list of currently selected values.
+ * @param {Function} props.handleValueClick - The function to handle click events on the values.
+>>>>>>> b525b17 (added documentation to TestCard.jsx)
  */
 export const TenValues = ({ stateOneValues, selectedValues, handleValueClick }) => {
     return (
@@ -66,7 +90,11 @@ export const TenValues = ({ stateOneValues, selectedValues, handleValueClick }) 
                         key={value}
                         value={value}
                         isSelected={selectedValues.includes(value)}
+<<<<<<< HEAD
                         onClick={() => handleValueClick(value)}
+=======
+                        onClick={handleValueClick}
+>>>>>>> b525b17 (added documentation to TestCard.jsx)
                     />
                 ))}
             </div>
@@ -75,11 +103,19 @@ export const TenValues = ({ stateOneValues, selectedValues, handleValueClick }) 
 };
 
 /**
+<<<<<<< HEAD
  * Component to display and allow selection from the second set of chosen values.
  * @param {Object} props - Component props.
  * @param {Array} props.stateTwoValues - Array of values selected totalling 10.
  * @param {Array} props.selectedValues - Array of currently selected values, specifically 5.
  * @param {Function} props.handleValueClick - Function to handle the selection of a value.
+=======
+ * FiveValues component renders the selected values for the user to narrow down to 5.
+ * @param {Object} props - The properties passed to the component.
+ * @param {Array} props.stateTwoValues - The list of values selected in the second step.
+ * @param {Array} props.selectedValues - The list of currently selected values.
+ * @param {Function} props.handleValueClick - The function to handle click events on the values.
+>>>>>>> b525b17 (added documentation to TestCard.jsx)
  */
 export const FiveValues = ({ stateTwoValues, selectedValues, handleValueClick }) => {
     return (
@@ -91,7 +127,11 @@ export const FiveValues = ({ stateTwoValues, selectedValues, handleValueClick })
                         key={value}
                         value={value}
                         isSelected={selectedValues.includes(value)}
+<<<<<<< HEAD
                         onClick={() => handleValueClick(value)}
+=======
+                        onClick={handleValueClick}
+>>>>>>> b525b17 (added documentation to TestCard.jsx)
                     />
                 ))}
             </div>
@@ -100,11 +140,19 @@ export const FiveValues = ({ stateTwoValues, selectedValues, handleValueClick })
 };
 
 /**
+<<<<<<< HEAD
  * Component to display and allow selection from the third set of chosen values.
  * @param {Object} props - Component props.
  * @param {Array} props.stateThreeValues - Array of values selected totalling 5.
  * @param {Array} props.selectedValues - Array of currently selected values, specifically 3.
  * @param {Function} props.handleValueClick - Function to handle the selection of a value.
+=======
+ * ThreeValues component renders the selected values for the user to narrow down to 3.
+ * @param {Object} props - The properties passed to the component.
+ * @param {Array} props.stateThreeValues - The list of values selected in the third step.
+ * @param {Array} props.selectedValues - The list of currently selected values.
+ * @param {Function} props.handleValueClick - The function to handle click events on the values.
+>>>>>>> b525b17 (added documentation to TestCard.jsx)
  */
 export const ThreeValues = ({ stateThreeValues, selectedValues, handleValueClick }) => {
     return (
@@ -116,10 +164,18 @@ export const ThreeValues = ({ stateThreeValues, selectedValues, handleValueClick
                         key={value}
                         value={value}
                         isSelected={selectedValues.includes(value)}
+<<<<<<< HEAD
                         onClick={() => handleValueClick(value)}
+=======
+                        onClick={handleValueClick}
+>>>>>>> b525b17 (added documentation to TestCard.jsx)
                     />
                 ))}
             </div>
         </>
     );
+<<<<<<< HEAD
 };
+=======
+};
+>>>>>>> b525b17 (added documentation to TestCard.jsx)
