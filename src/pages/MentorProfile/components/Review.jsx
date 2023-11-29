66d4c05ea8d7bcac4a1667df2a77873e064d2898
Jayden <Jayden7evans@gmail.com>
2023-11-29 13:07:26 -0800

    A few fixes. Adding more data

--- src/pages/MentorProfile/components/Review.jsx
+++ src/pages/MentorProfile/components/Review.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 
-const Review = () => {
+const Review = (props) => {
   return (
     <div className='w-5/12 my-5 mr-5'>
             <div className='flex flex-col gap-2'>
                 <div className='flex w-full justify-between'>
-                    <p className='font-semibold'>First Name</p>
-                    <p>X/X/2023</p>
+                    <p className='font-semibold'>{props.name}</p>
+                    <p>{props.date}</p>
                 </div>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Tenetur delectus dolore rem nemo magni autem iure ipsa assumenda,
-                    earum mollitia impedit veritatis. Pariatur numquam, rem nisi
-                    cumque officia labore modi.
+                    {props.review}
                 </p>
             </div>
     </div>
