a5a359d1e390403d3fa112c145d932eb22676a95
lureeves <ljr77777@gmail.com>
2023-11-28 21:20:00 -0800

    Updated mentor cards styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
       extend: {
+        width: {
+            'max-content': 'max-content',
+          },
         fontFamily: {
             'sans': ["Open Sans"],
         },
