3e705eac5afc867fcb8fb417ece224c40b8a84d5
lureeves <ljr77777@gmail.com>
2023-11-29 12:35:32 -0800

    added glass icon component

--- src/pages/Home/components/RoleSearch.jsx
+++ src/pages/Home/components/RoleSearch.jsx
@@ -1,24 +1,32 @@
-import React, { useState } from 'react';
-import { MagGlass, arrow, filter } from '../../../assets';
+import React, { useState, useEffect } from 'react';
+import MagGlassIcon from './subComponents/MagGlassIcon.jsx';
 import { MentorData } from '../../../data/GoogleDriveMentors.jsx';
-
 
 const RoleSearch = () => {
   const [searchTerm, setSearchTerm] = useState('');
 
+  useEffect(() => {
+    const results = MentorData.filter((mentor) => mentor.role.toLowerCase().includes(searchTerm.toLowerCase()));
+    for (const filteredMentor of results) {
+        console.log(filteredMentor.first_name + ' ' + filteredMentor.last_name + ' - ' + filteredMentor.role);
+        break; // This will stop the loop after the first iteration
+    }
+    console.log('--- End of set ---');
+}, [searchTerm]);
+
   return (
     <div className='filter-container flex justify-between items-center w-56 h-10 py-3 pr-6 pl-7 gap-3'>
-      <img src={MagGlass} alt="" />
-      <input 
-        className="w-full p-1" 
-        type="text" 
-        placeholder='Search Role'
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      {MentorData.filter((mentor) => mentor.role.toLowerCase().includes(searchTerm.toLowerCase())).map((filteredMentor, index) => {
-        console.log(filteredMentor.first_name + ' ' + filteredMentor.last_name + ' - ' + filteredMentor.role);
-      })}
+
+        <MagGlassIcon />
+
+
+        <input 
+            className="w-full p-1" 
+            type="text" 
+            placeholder='Search Role'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+        />
     </div>
   );
 };
